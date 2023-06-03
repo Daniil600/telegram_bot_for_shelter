b7d5d6f383f4c5182ab6002a3e10dfa7eca6390b
Daniil600 <ter6062@ya.ru>
2023-06-03 14:48:29 +0300

    Initial commit

---
+++ src/main/java/skypro/teamwork/telegram_bot_for_shelter/TelegramBotForShelterApplication.java
@@ -0,0 +1,13 @@
+package skypro.teamwork.telegram_bot_for_shelter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramBotForShelterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelegramBotForShelterApplication.class, args);
+    }
+
+}
