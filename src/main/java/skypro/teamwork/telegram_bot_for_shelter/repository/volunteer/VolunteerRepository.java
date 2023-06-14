5925def2b91602cc154651b5124b4d92aedafcf4
Рамиля <bzzik@list.ru>
2023-06-14 23:20:23 +0300

    добавила javadoc описание

--- src/main/java/skypro/teamwork/telegram_bot_for_shelter/repository/volunteer/VolunteerRepository.java
+++ src/main/java/skypro/teamwork/telegram_bot_for_shelter/repository/volunteer/VolunteerRepository.java
@@ -3,6 +3,10 @@ package skypro.teamwork.telegram_bot_for_shelter.repository.volunteer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import skypro.teamwork.telegram_bot_for_shelter.model.volunteer.Volunteer;
+
+/**
+ * репозиторий для хранения волонтёров
+ */
 @Repository
 public interface VolunteerRepository extends JpaRepository<Volunteer, Long> {
 }
