dc8077f0482e08dcf223e2665b2f9531526d1c23
Рамиля <bzzik@list.ru>
2023-06-14 22:54:01 +0300

    описание отчета по диете, здоровью и привычкам объединила в одно описание - rept, добавила javadoc описание

--- src/main/java/skypro/teamwork/telegram_bot_for_shelter/model/pet/ReportPet.java
+++ src/main/java/skypro/teamwork/telegram_bot_for_shelter/model/pet/ReportPet.java
@@ -3,27 +3,36 @@ package skypro.teamwork.telegram_bot_for_shelter.model.pet;
 import javax.persistence.*;
 import java.util.Objects;
 
+/**
+ * Этот класс описывает таблицу report_pet
+ * в него складываются отчеты по питомцам, присланные усыновителями животных в течение испытательного срока
+ */
 @Entity
 @Table(name = "report_pet")
 public class ReportPet {
     @Id
     @GeneratedValue(generator = "report_pet_seq")
     private long id;
+
+    /**
+     * к каждому отчету прикладывается фото питомца, зависимость OneToOne
+     */
     @OneToOne
     private PhotoPetReport photoPetReport;
-    private String diet;
-    private String health;
-    private String habits;
+    private String rept;
+
+    /**
+     * Отчет привязан к конкретному питомцу, у каждого питомца множество отчетов, зависимость ManyToOne
+     * привязка к колонке pet_id из таблицы pet
+     */
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "pet_id")
     private Pet pet;
 
-    public ReportPet(long id, PhotoPetReport photoPetReport, String diet, String health, String habits, Pet pet) {
+    public ReportPet(long id, PhotoPetReport photoPetReport, String rept, Pet pet) {
         this.id = id;
         this.photoPetReport = photoPetReport;
-        this.diet = diet;
-        this.health = health;
-        this.habits = habits;
+        this.rept = rept;
         this.pet = pet;
     }
 
@@ -46,28 +55,12 @@ public class ReportPet {
         this.photoPetReport = photoPetReport;
     }
 
-    public String getDiet() {
-        return diet;
+    public String getRept() {
+        return rept;
     }
 
-    public void setDiet(String diet) {
-        this.diet = diet;
-    }
-
-    public String getHealth() {
-        return health;
-    }
-
-    public void setHealth(String health) {
-        this.health = health;
-    }
-
-    public String getHabits() {
-        return habits;
-    }
-
-    public void setHabits(String habits) {
-        this.habits = habits;
+    public void setRept(String rept) {
+        this.rept = rept;
     }
 
     public Pet getPet() {
@@ -83,21 +76,19 @@ public class ReportPet {
         if (this == o) return true;
         if (!(o instanceof ReportPet)) return false;
         ReportPet reportPet = (ReportPet) o;
-        return Objects.equals(photoPetReport, reportPet.photoPetReport) && Objects.equals(diet, reportPet.diet) && Objects.equals(health, reportPet.health) && Objects.equals(habits, reportPet.habits) && Objects.equals(pet, reportPet.pet);
+        return Objects.equals(photoPetReport, reportPet.photoPetReport) && Objects.equals(rept, reportPet.rept) && Objects.equals(pet, reportPet.pet);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(photoPetReport, diet, health, habits, pet);
+        return Objects.hash(photoPetReport, rept, pet);
     }
 
     @Override
     public String toString() {
         return "ReportPet{" +
                 "photoPetsReport=" + photoPetReport+
-                ", diet='" + diet + '\'' +
-                ", health='" + health + '\'' +
-                ", habits='" + habits + '\'' +
+                ", rept='" + rept + '\'' +
                 ", pet=" + pet +
                 '}';
     }
